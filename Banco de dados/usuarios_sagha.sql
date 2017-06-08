2f63d57f71829993066071e432913782ae502a52
mateushtoledo <mateushtoledo@gmail.com>
2017-06-08 20:08:46 -0300

    Atualizando arquivo com a tabela de usuarios do banco

--- Banco de dados/usuarios_sagha.sql
+++ Banco de dados/usuarios_sagha.sql
@@ -1,11 +1,9 @@
 create table usuarios
 (
 	nome varchar(64),
-	cpf char(11) unique,
+	cpf char(11) primary key,
 	funcao varchar(16),
-	login varchar(16),
-	senha char(8),
-	primary key (login,senha)
+	senha char(8)
 );
 
-insert into usuarios values('Mateus Henrique Toledo','12332112321','AGENTE','root','root0'),('Carlos Henrique Reis','40028922123','AGENTE','root1','root2'),('Victor Rodrigues','08007777000','AGENTE','root','root2');
+insert into usuarios values('Jean Carlos','00000000001','GERENTE','JEAN'),('Victor Rodrigues','00000000002','AGENTE','VICTOR'),('Mario mandzukic','00000000003','GERENTE','MARIO'),('Carlos Henrique','00000000004','GERENTE','CARLOS');
